3443ed610670b8d1bd1593d2192ce79c11cb05d3
Enis Ozgit <eozgit@gmail.com>
2020-04-21 15:05:01 +0100

    Create app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace core_mvc_kanban.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
