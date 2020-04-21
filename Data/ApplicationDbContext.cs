3f0d79b3b1c981dcff733c1cb31e0258e05eeb56
Enis Ozgit <eozgit@gmail.com>
2020-04-21 18:17:43 +0100

    Add migration for project model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using QuakeKanban.Models;
 
 namespace QuakeKanban.Data
 {
@@ -12,5 +13,7 @@ namespace QuakeKanban.Data
             : base(options)
         {
         }
+
+        public DbSet<Project> Project { get; set; }
     }
 }
---
+++ Data/Migrations/20200421171229_AddProject.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuakeKanban.Data.Migrations
+{
+    public partial class AddProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false),
+                    Description = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Project");
+        }
+    }
+}
